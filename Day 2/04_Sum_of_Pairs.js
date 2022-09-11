64b84acf996e99117570741fc109cc59288f8dcc
souhail bouricha <souhailbourigha@gmail.com>
2022-09-11 23:37:43 +0100

    Solve some katas

--- Day 2/04_Sum_of_Pairs.js
+++ Day 2/04_Sum_of_Pairs.js
@@ -1,7 +1,7 @@
 // Given a list of integers and a single sum value, return the first two values 
 // (parse from the left please) in order of appearance that add up to form the sum.
 // If there are two or more pairs with the required sum, the pair whose second 
-// element has the smallest index is the solution.
+// element has the smallest index is the solution .
 
 
 // sum_pairs([11, 3, 7, 5],         10)
@@ -26,4 +26,4 @@
 // == [3, 7]
 function sumPairs(ints, s) {
     return [0, 0] || undefined;
-}
+} 

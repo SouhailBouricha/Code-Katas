e0c1711c61ee55368a2edad2bd8d584c6fa6a981
souhail bouricha <souhailbourigha@gmail.com>
2022-09-11 23:36:33 +0100

    Solve some katas

--- Day 2/04_Sum_of_Pairs.js
+++ Day 2/04_Sum_of_Pairs.js
@@ -1,3 +1,8 @@
+// Given a list of integers and a single sum value, return the first two values 
+// (parse from the left please) in order of appearance that add up to form the sum.
+// If there are two or more pairs with the required sum, the pair whose second 
+// element has the smallest index is the solution.
+
 function sumPairs(ints, s) {
     return [0, 0] || undefined;
-  }
+}

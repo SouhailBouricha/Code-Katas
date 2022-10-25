ccc42a100bbe4c428c85dd38de84e14f0950b3d4
souhail bouricha <souhailbourigha@gmail.com>
2022-10-25 18:12:36 +0100

    Day 4

---
+++ Day 4/05_Maximum_subarray_sum.js
@@ -0,0 +1,16 @@
+var maxSequence = function(array){
+    let max = 0
+    for (let i = 0; i <= array.length; i++) {
+        for (let left = 0, right = i; left < array.length, right < array.length; left++, right++) {
+            let element = 0;
+            for (let j = 0; j < array.slice(left, right + 1).length; j++) {
+                element = element + array.slice(left, right + 1)[j];
+            }
+            if(max < element){
+                max = element
+            }
+        }
+    }
+    return max;
+}
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])

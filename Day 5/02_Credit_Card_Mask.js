c431bb43aff1062fd06c142081d04540b00e6eba
souhail bouricha <souhailbourigha@gmail.com>
2022-11-04 20:27:57 +0100

    Day 5

---
+++ Day 5/02_Credit_Card_Mask.js
@@ -0,0 +1,23 @@
+// return masked string
+function maskify(cc) {
+    if(cc.length > 4){
+        let id = 4;
+        let newWord = "";
+        for (let i = cc.length - 1; i >= 0; i--) {
+            if(id > 0){
+                newWord = cc[i] + newWord;
+            }
+            else{
+                newWord =  "#" + newWord;
+            }
+            id--;
+        }
+        console.log(newWord);
+    }
+    else{
+        console.log(cc);
+    }
+}
+maskify('4556364607935616') // ############5616
+maskify('1') // 1
+maskify('11111') // #1111

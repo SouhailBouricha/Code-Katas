82a021c95d24f5955b61df1f3e30d80fb2c80b4c
souhail bouricha <souhailbourigha@gmail.com>
2022-11-07 19:45:28 +0100

    Day 5

--- Day 5/04_Equal_Sides_Of_An_Array.js
+++ Day 5/04_Equal_Sides_Of_An_Array.js
@@ -1,4 +1,12 @@
-function findEvenIndex(arr)
-{
-  //Code goes here!
+function findEvenIndex(arr){
+    let leftSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const element = array[i];
+        console.log(arr.slice(i));
+    }
 }
+
+findEvenIndex([1,2,3,4,3,2,1]) //3
+findEvenIndex([1,100,50,-51,1,1]) //1
+findEvenIndex([1,2,3,4,5,6]) //-1
+findEvenIndex([20,10,30,10,10,15,35]) //3

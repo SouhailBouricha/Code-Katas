e59f54c9d3e4942e4f06ebd15de6fe9f6291e724
souhail bouricha <souhailbourigha@gmail.com>
2022-11-07 19:36:06 +0100

    Day 5

--- Day 5/03_Create_Phone_Number.js
+++ Day 5/03_Create_Phone_Number.js
@@ -1,6 +1,6 @@
 function createPhoneNumber(numbers){
     let phoneNumber = `(${numbers.slice(0,3).join("")}) ${numbers.slice(3,6).join("")}-${numbers.slice(6,10).join("")}`;
-    console.log(phoneNumber);
+    return phoneNumber;
 }
 
 

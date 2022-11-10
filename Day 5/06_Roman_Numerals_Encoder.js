7dc011b54559cc02cb21c2c2abc0b5becb75da3f
souhail bouricha <souhailbourigha@gmail.com>
2022-11-10 17:51:20 +0100

    Day 5

---
+++ Day 5/06_Roman_Numerals_Encoder.js
@@ -0,0 +1,12 @@
+
+
+solution(1)  // 'I', '1 should return "I"'
+solution(2)  // 'II', '2 should return "II"'
+solution(3)  // 'III', '3 should return "III"'
+solution(4)  // 'IV', '4 should return "IV"'
+solution(5)  // 'V', '5 should return "V"'
+solution(9)  // 'IX', '9 should return "IX"'
+solution(11) //  'XI', '11 should return "XI"'
+solution(19) //  'XIX', '19 should return "XIX"'
+solution(22) //  'XXII', '22 should return "XXII"'
+solution(15) //  'XV', '15 should return "XV"'

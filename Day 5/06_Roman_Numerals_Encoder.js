991bf460ce880ad6040cd58949ce417a4bdb843b
souhail bouricha <souhailbourigha@gmail.com>
2022-11-11 20:53:41 +0100

    Day 5

--- Day 5/06_Roman_Numerals_Encoder.js
+++ Day 5/06_Roman_Numerals_Encoder.js
@@ -10,7 +10,6 @@ function solution(number){
         8 : "VIII",
         9 : "IX",
         10 : "X",
-        
         20 : "XX",
         30 : "XXX",
         40 : "XL",
@@ -20,7 +19,6 @@ function solution(number){
         80 : "LXXX",
         90 : "XC",
         100 : "C",
-
         200	 : "CC",	
         300	 : "CCC",
         400	 : "CD",	
@@ -31,15 +29,26 @@ function solution(number){
         900	 : "CM",
         1000 : "M",
     }
+    romanNumber = "";
+    for(let i = Object.keys(lockUp).length - 1; i > 0; i--){
+        if(number >= Object.keys(lockUp)[i]){
+            romanNumber += lockUp[Object.keys(lockUp)[i]];
+            number -=  Object.keys(lockUp)[i];
+            console.log(number,romanNumber);
+        }
+        // console.log(lockUp[Object.keys(lockUp)[i]]);
+    }
+    console.log(romanNumber);
 }
 
-solution(1)  // 'I', '1 should return "I"'
-solution(2)  // 'II', '2 should return "II"'
-solution(3)  // 'III', '3 should return "III"'
-solution(4)  // 'IV', '4 should return "IV"'
-solution(5)  // 'V', '5 should return "V"'
-solution(9)  // 'IX', '9 should return "IX"'
-solution(11) //  'XI', '11 should return "XI"'
-solution(19) //  'XIX', '19 should return "XIX"'
-solution(22) //  'XXII', '22 should return "XXII"'
-solution(15) //  'XV', '15 should return "XV"'
+//solution(1)  // 'I', '1 should return "I"'
+solution(1001)
+//solution(2)  // 'II', '2 should return "II"'
+//solution(3)  // 'III', '3 should return "III"'
+//solution(4)  // 'IV', '4 should return "IV"'
+//solution(5)  // 'V', '5 should return "V"'
+//solution(9)  // 'IX', '9 should return "IX"'
+//solution(11) //  'XI', '11 should return "XI"'
+//solution(19) //  'XIX', '19 should return "XIX"'
+//solution(22) //  'XXII', '22 should return "XXII"'
+//solution(15) //  'XV', '15 should return "XV"'

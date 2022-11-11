df10cb0440e70ab54d8d03a65d210c18c588c02c
souhail bouricha <souhailbourigha@gmail.com>
2022-11-11 21:50:00 +0100

    Day 5

--- Day 5/06_Roman_Numerals_Encoder.js
+++ Day 5/06_Roman_Numerals_Encoder.js
@@ -35,26 +35,23 @@ function solution(number){
             while(number >= Object.keys(lockUp)[i]){
                 romanNumber += lockUp[Object.keys(lockUp)[i]];
                 number = number - Object.keys(lockUp)[i];
-                // console.log(number,romanNumber,);
             }
         }
-        // console.log(number,Object.keys(lockUp)[i],lockUp[Object.keys(lockUp)[i]]);
-        // console.log(lockUp[Object.keys(lockUp)[i]]);
     }
     console.log(romanNumber);
 }
-//2007 should, "MMVII": expected 'MCMCVII' to equal 'MMVII'
-solution(2007)
-solution(2000)
 
 
-//solution(1)  // 'I', '1 should return "I"'
-//solution(2)  // 'II', '2 should return "II"'
-//solution(3)  // 'III', '3 should return "III"'
-//solution(4)  // 'IV', '4 should return "IV"'
-//solution(5)  // 'V', '5 should return "V"'
-//solution(9)  // 'IX', '9 should return "IX"'
-//solution(11) //  'XI', '11 should return "XI"'
-//solution(19) //  'XIX', '19 should return "XIX"'
-//solution(22) //  'XXII', '22 should return "XXII"'
-//solution(15) //  'XV', '15 should return "XV"'
+
+solution(1)  // 'I', '1 should return "I"'
+solution(2)  // 'II', '2 should return "II"'
+solution(3)  // 'III', '3 should return "III"'
+solution(4)  // 'IV', '4 should return "IV"'
+solution(5)  // 'V', '5 should return "V"'
+solution(9)  // 'IX', '9 should return "IX"'
+solution(11) //  'XI', '11 should return "XI"'
+solution(19) //  'XIX', '19 should return "XIX"'
+solution(22) //  'XXII', '22 should return "XXII"'
+solution(15) //  'XV', '15 should return "XV"'
+solution(2000) // 'MM', '2000 should return "MM"'
+solution(2007) // 'MMVII', '2007 should return "MMVII"'

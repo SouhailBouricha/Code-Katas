417757372a6569046f917ca0c896544f2eba5783
souhail bouricha <souhailbourigha@gmail.com>
2022-11-15 22:08:18 +0100

    Day 6

---
+++ Day 6/03_Sum_of_Digits_Digital_Root.js
@@ -0,0 +1,15 @@
+function digitalRoot(n) {
+    let num = n;
+    while(String(num).length > 1){
+        let newNum = 0; 
+        for (let i = 0; i < String(num).length; i++) {
+            const element = String(num)[i];
+            newNum += Number(element); 
+        }
+        num = newNum;
+    }
+    return num;
+}
+
+console.log(digitalRoot(16), 7 );
+console.log(digitalRoot(456), 6 );

0c08769a8309cdc217d262a1aa9e881e16f3ab5c
souhail bouricha <souhailbourigha@gmail.com>
2022-11-16 21:23:46 +0100

    Day 6

---
+++ Day 6/06_Calculating_with_Functions.js
@@ -0,0 +1,21 @@
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
+
+
+console.log(seven(times(five ())), 35,seven(times(five ())) === 35);
+console.log(four (plus (nine ())), 13,four (plus (nine ())) === 13);
+console.log(eight(minus(three())),  5,eight(minus(three())) ===  5);
+console.log(six(dividedBy(two())),  3,six(dividedBy(two())) ===  3);

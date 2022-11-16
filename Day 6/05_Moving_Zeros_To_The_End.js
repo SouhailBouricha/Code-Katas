6fe5c48b55b3c707c3241946632cc77f9c1b141d
souhail bouricha <souhailbourigha@gmail.com>
2022-11-16 20:29:37 +0100

    Day 6

---
+++ Day 6/05_Moving_Zeros_To_The_End.js
@@ -0,0 +1,6 @@
+function moveZeros(arr) {
+    return
+}
+
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]), [1, 2, 1, 1, 3, 1, 0, 0, 0, 0],moveZeros([1,2,0,1,0,1,0,3,0,1]) == [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]) ,[false,1,1,2,1,3,"a",0,0],moveZeros([false,1,0,1,2,0,1,3,"a"]) == [false,1,1,2,1,3,"a",0,0]);

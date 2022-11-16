2791ae6644655b654e4ec02e82d72ff774c9bd57
souhail bouricha <souhailbourigha@gmail.com>
2022-11-16 20:54:09 +0100

    Day 6

--- Day 6/05_Moving_Zeros_To_The_End.js
+++ Day 6/05_Moving_Zeros_To_The_End.js
@@ -1,9 +1,9 @@
 function moveZeros(arr) {
     let realArr = [];
     let zerosArr = [];
-    
-    return
+    arr.forEach(element => {element !== 0 ? realArr.push(element) : zerosArr.push(element) });
+    return [...realArr,...zerosArr];
 }
 
-console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]), [1, 2, 1, 1, 3, 1, 0, 0, 0, 0],moveZeros([1,2,0,1,0,1,0,3,0,1]) == [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
-console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]) ,[false,1,1,2,1,3,"a",0,0],moveZeros([false,1,0,1,2,0,1,3,"a"]) == [false,1,1,2,1,3,"a",0,0]);
+console.log(JSON.stringify(moveZeros([1,2,0,1,0,1,0,3,0,1])), JSON.stringify([1, 2, 1, 1, 3, 1, 0, 0, 0, 0]));
+console.log(JSON.stringify(moveZeros([false,1,0,1,2,0,1,3,"a"])) ,JSON.stringify([false,1,1,2,1,3,"a",0,0]));

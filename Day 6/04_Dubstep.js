2f76981d7f55e3b7e450f8136b2f272114c03d30
souhail bouricha <souhailbourigha@gmail.com>
2022-11-16 19:00:23 +0100

    Day 6

--- Day 6/04_Dubstep.js
+++ Day 6/04_Dubstep.js
@@ -17,6 +17,4 @@ console.log(songDecoder("AWUBBWUBC"), "A B C", songDecoder("AWUBBWUBC") == "A B 
 console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"), "A B C",songDecoder("AWUBWUBWUBBWUBWUBWUBC") == "A B C");
 console.log(songDecoder("WUBAWUBBWUBCWUB"), "A B C",songDecoder("WUBAWUBBWUBCWUB") == "A B C");
 
-// console.log(songDecoder("AWUBBWUBC"), "A B C","WUB should be replaced by 1 space");
-// console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"), "A B C","multiples WUB should be replaced by only 1 space");
-// console.log(songDecoder("WUBAWUBBWUBCWUB"), "A B C","heading or trailing spaces should be removed");
+

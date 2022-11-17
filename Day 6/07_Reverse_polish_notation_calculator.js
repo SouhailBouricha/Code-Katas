6e36dca9e23a75cd993bfa9048b59b3c89dc32c2
souhail bouricha <souhailbourigha@gmail.com>
2022-11-17 22:43:18 +0100

    Day 6

--- Day 6/07_Reverse_polish_notation_calculator.js
+++ Day 6/07_Reverse_polish_notation_calculator.js
@@ -1,20 +1,24 @@
 function calc(expr) {
-    if(expr.split(' ').length === 1){
-        return +expr.split(' ')[0]
-    }
-    else{
-        let result = 0;
-        for (let i = 0; i < expr.split(' ').length; i++) {
-            const element = expr.split(' ')[i];
-            let start = 0;
-            if(["-","+","/","*"].includes(element)){
-                result = result + expr.split(' ').slice(start,i).reduce((res,ele) =>  res += +ele ,0);
-                start = i;
-            }
+   let inputs = [...expr.split(" ")];
+   let stuck = [];
+   let operations = {
+    "+" : (a,b) => a + b,
+    "*" : (a,b) => a * b,
+    "-" : (a,b) => a - b,
+    "/" : (a,b) => a / b,
+   }
+   for (let i = 0; i < inputs.length; i++) {
+        if(operations[inputs[i]]){
+            let opi = inputs[i];
+            let right = stuck.pop();
+            let left = stuck.pop();
+            stuck.push(operations[opi](+left,+right));
         }
-        return result;
-    }
-    return expr.split(" ");
+        else{
+            stuck.push(inputs[i]);
+        }
+   }
+   return +stuck;
 }
 
 
@@ -22,6 +26,6 @@ console.log(calc(""), 0, "Should work with empty string",calc("") === 0);
 console.log(calc("3"), 3, "Should parse numbers",calc("3") === 3);
 console.log(calc("3.5"), 3.5, "Should parse float numbers",calc("3.5") === 3.5);
 console.log(calc("1 3 +"), 4, "Should support addition",calc("1 3 +") === 4);
-// console.log(calc("1 3 *"), 3, "Should support multiplication",calc("1 3 *") === 3);
-// console.log(calc("1 3 -"), -2, "Should support subtraction",calc("1 3 -") === -2);
-// console.log(calc("4 2 /"), 2, "Should support division",calc("4 2 /") === 2);
+console.log(calc("1 3 *"), 3, "Should support multiplication",calc("1 3 *") === 3);
+console.log(calc("1 3 -"), -2, "Should support subtraction",calc("1 3 -") === -2);
+console.log(calc("4 2 /"), 2, "Should support division",calc("4 2 /") === 2);

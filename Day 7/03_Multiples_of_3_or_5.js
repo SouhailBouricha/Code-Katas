d7554329cc3972448b88e1c2902f28823acb1d67
souhail bouricha <souhailbourigha@gmail.com>
2022-11-21 19:47:04 +0100

    Day 7

--- Day 7/03_Multiples_of_3_or_5.js
+++ Day 7/03_Multiples_of_3_or_5.js
@@ -1,5 +1,14 @@
 function solution(number){
-  
+  let numbers = [];
+  for (let i = 1; i < number; i++) {
+    if(i % 5 === 0){
+        numbers.push(i)
+    }
+    else if(i % 3 === 0){
+        numbers.push(i)
+    }
+  }
+  return numbers.reduce((sum,element) => sum += element,0);
 }
 
 

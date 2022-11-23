7c5226b02bf4fbe41d59b322420e49b39724e157
souhail bouricha <souhailbourigha@gmail.com>
2022-11-23 16:49:40 +0100

    Day 7

--- Day 7/06_RGB_To_Hex_Conversion.js
+++ Day 7/06_RGB_To_Hex_Conversion.js
@@ -1,3 +1,21 @@
 function rgb(r, g, b){
-    // complete this function  
+    let arr = [r,g,b];
+    let hex = "";
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+        element > 250 ? element = 255 : element = element;
+        element < 0 ? element = 0 : element = element;
+        if(element.toString(16).length === 1){
+            hex = hex + element.toString(16) + element.toString(16);
+        }
+        else{
+            hex = hex + element.toString(16);
+        }
+    } 
+    return hex.toUpperCase();
 }
+
+console.log(rgb(0, 0, 0), '000000')
+console.log(rgb(0, 0, -20), '000000')
+console.log(rgb(300,255,255), 'FFFFFF')
+console.log(rgb(173,255,47), 'ADFF2F')

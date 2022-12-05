18bafee86770e0b76b1495a1cc166b7cd3be4994
souhail bouricha <souhailbourigha@gmail.com>
2022-12-05 19:28:10 +0100

    Day 9

---
+++ Day 9/01_Correct_the_mistakes_of_the_character_recognition_software.js
@@ -0,0 +1,9 @@
+function correct(string){
+	
+}
+
+console.log(correct("L0ND0N"),"LONDON");
+console.log(correct("51NGAP0RE"),"SINGAPORE");
+console.log(correct("DUBL1N"),"DUBLIN");
+console.log(correct("BUDAPE5T"),"BUDAPEST");
+console.log(correct("PAR15"),"PARIS");

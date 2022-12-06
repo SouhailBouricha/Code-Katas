dead79eb2b94a0cc5222b415b579b6a352082180
souhail bouricha <souhailbourigha@gmail.com>
2022-12-06 02:46:02 +0100

    Day 9

---
+++ Day 9/04_Nesting_Structure_Comparison.js
@@ -0,0 +1,20 @@
+function isArray(array) {
+    return array.constructor === Array;
+  }
+  Array.prototype.sameStructureAs = function (other) {
+       if (!isArray(other)) return false;
+    if (this.length != other.length) return false;
+  
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i];
+      const otherElement = other[i];
+      if (isArray(element) && !element.sameStructureAs(otherElement)) {
+        return false;
+      } else if (!isArray(element) && isArray(otherElement)) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
+  

7f5563cd7f164f45558e188dcb5a9a191c26744d
souhail bouricha <souhailbourigha@gmail.com>
2022-12-06 01:30:33 +0100

    Day 9

--- Day 9/02_Does_my_number_look_big_in_this.js
+++ Day 9/02_Does_my_number_look_big_in_this.js
@@ -1,3 +1,7 @@
 function narcissistic(value) {
-    // Code me to return true or 
+    let base = String(value).length;
+    let Result = String(value).split("").reduce((sum,element) => sum += (Number(element) ** base), 0);
+    return value === Result ? true : false;
 }
+console.log(narcissistic(7), true, "7 is narcissistic" );
+console.log(narcissistic(371), true, "371 is narcissistic" );

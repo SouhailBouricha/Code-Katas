8d166d408ae79b8fda1e833f56e22ab3f759a031
souhail bouricha <souhailbourigha@gmail.com>
2022-12-13 18:08:46 +0100

    Day 10

--- Day 10/03_Pete_the_baker.js
+++ Day 10/03_Pete_the_baker.js
@@ -1,21 +1,22 @@
 function cakes(recipe, available) {
     let count = 0;
     let thin = true;
+    let numberLength = Object.keys(recipe).length > Object.keys(available).length ? Object.keys(recipe).length : Object.keys(available).length;
     while(thin === true){
-    for (let i = 0; i < Object.keys(recipe).length; i++) {
-        const element = recipe[Object.keys(recipe)[i]];
-        const element_2 = available[Object.keys(recipe)[i]];
-        if(element > element_2){
-            thin = false;
-        }
-        else{
-            available[Object.keys(recipe)[i]] = available[Object.keys(recipe)[i]] - element; 
-        }
-    }
-    count++;
+        for (let i = 0; i < numberLength; i++) {
+            const recipeElement = recipe[Object.keys(recipe)[i]] ? recipe[Object.keys(recipe)[i]] : 0;
+            const availableElement = available[Object.keys(available)[i]] ? available[Object.keys(available)[i]] : 0;
+            if(recipeElement <= availableElement){
+                available[Object.keys(available)[i]] = available[Object.keys(available)[i]] - recipeElement;
+            }
+            else{
+                thin = false;
+            }
+            // console.log(recipeElement,availableElement,available,count);        
+        } 
+        count++;
     }
     return count;
-
 }
 
 
@@ -26,4 +27,4 @@ console.log(cakes(recipe, available), 2);
 
 recipe = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
 available = {sugar: 500, flour: 2000, milk: 2000};
-console.log(cakes(recipe, available), 0);
+console.log(cakes(recipe, available),0);
